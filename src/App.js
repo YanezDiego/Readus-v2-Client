6dbaec1ed50598b9afd9946747ffc59a8e756396
Diego Yanez <diegoyanez@MacBook-Pro-9.fios-router.home>
2019-05-20 18:00:50 -0400

    add route & import landing component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import  { BrowserRouter as Router, Route} from 'react-router-dom'
 import './App.css';
 
 
-import NavBar from './component/Navbar'
+import NavBar from './component/layout/Navbar'
+import Landing from './component/layout/Landing'
 
 function App() {
   return (
     <div className="App">
     <Router>
       <NavBar/>
+      <Route exact path = "/" component={Landing}/>
     </Router>
     </div>
   );
