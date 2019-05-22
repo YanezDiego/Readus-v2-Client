9897e84e355f63eab546976b8567b8cedea77c5f
Diego Yanez <diegoyanez@MacBook-Pro-9.fios-router.home>
2019-05-21 21:15:19 -0400

    add component to handle render of topstories

---
+++ src/component/TopStories/TopStoriesList.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+
+class TopStoriesList extends Component{
+
+
+
+    render(){
+        return(
+            <div>
+                Hi
+            </div>
+        )
+    }
+}
+
+
+export default TopStoriesList
