0c1afa65b2f0b0d326851cdf509c6be7f1c06b42
Diego Yanez <diegoyanez@MacBook-Pro-9.fios-router.home>
2019-05-21 22:50:33 -0400

    render in row of three

---
+++ src/component/TopStories/TopStorie.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+
+const TopStorie = (props) => {
+
+    return(
+         <div className = 'col s12 m6 l4'>
+            <div className='card hoverable'>
+                <div className='card-image waves-effect waves-block waves-light'>
+ 
+                    <img className= "activator" src = { `${props.storie["multimedia"][3]["url"]}` }/>
+                    <a className = "waves-effect right waves-light btn blue-grey lighten-5" > <i className = "material-icons red-text"> add </i></a>
+                </div>
+                <div className = "card-content">
+                    <span 
+                    className = "card-title truncate activator black-text"
+                    style={{
+                        fontSize: '25px'
+                    }}> 
+                        <i className = "material-icons right" > more_vert </i>
+                        { props.storie["title"] }
+                    </span>
+                    <p className = "truncate activator">{props.storie["abstract"]}</p>
+                </div>
+                <div className = "card-reveal">
+                   <span className = "card-title black-text">
+                        <i className = "material-icons right"> close </i>
+                        <a href={`${props.storie["short_url"]}`}>{ props.storie["title"] }</a>
+                   </span>
+                   <p> { props.storie["abstract"] }</p>
+                   <p> <b> { props.storie["byline"] } </b> </p>
+                </div>
+            </div>
+        </div>       
+    )
+}
+
+export default TopStorie
