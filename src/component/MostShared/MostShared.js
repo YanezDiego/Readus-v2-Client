fca8f19bf68e842f7973dd23d88ceb913036536a
Diego Yanez <diegoyanez@MacBook-Pro-9.fios-router.home>
2019-05-21 21:11:38 -0400

    fix where it was not rendering column correctly

--- src/component/MostShared/MostShared.js
+++ src/component/MostShared/MostShared.js
@@ -3,27 +3,27 @@ import React from 'react'
 
 const MostShared = (props) => {
     return(
-        <div className = 'col s12  l6'>
+        <div className = 'col s12 m6 l6'>
             <div className='card hoverable'>
                 <div className='card-image waves-effect waves-block waves-light'>
  
                     <img className= "activator" src = {`${props.shared['media'][0]["media-metadata"][2]['url']}`}/>
-                    <a class = "waves-effect right waves-light btn blue-grey lighten-5" > <i class = "material-icons red-text"> add </i></a>
+                    <a className = "waves-effect right waves-light btn blue-grey lighten-5" > <i className = "material-icons red-text"> add </i></a>
                 </div>
                 <div className = "card-content">
                     <span className = "card-title activator black-text"> 
-                    {props.shared["title"]}
-                    <i class = "material-icons right" > more_vert </i>
+                        <i className = "material-icons right" > more_vert </i>
+                        {props.shared["title"]}
                     </span>
-                    <p className="truncate activator">{props.shared["abstract"]}</p>
+                    <p className = "truncate activator">{props.shared["abstract"]}</p>
                 </div>
-                <div className="card-reveal">
-                   <span className = "card-title black-text" >
-                    <i class = "material-icons right"> close </i>
-                   {props.shared["title"]} 
+                <div className = "card-reveal">
+                   <span className = "card-title black-text">
+                        <i className = "material-icons right"> close </i>
+                        { props.shared["title"] }
                    </span>
-                   <p> {props.shared["abstract"]}</p>
-                   <p><b>{props.shared["byline"]}</b></p>
+                   <p> { props.shared["abstract"] }</p>
+                   <p> <b> { props.shared["byline"] } </b> </p>
                 </div>
             </div>
         </div>       
