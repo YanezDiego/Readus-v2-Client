1e56559b2f3fea91afd591e4fdb49389fa6e9427
Diego Yanez <diegoyanez@MacBook-Pro-9.fios-router.home>
2019-05-21 22:49:55 -0400

    render top stories correctly

--- src/component/TopStories/TopStoriesList.js
+++ src/component/TopStories/TopStoriesList.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 
-
+import TopStorie from './TopStorie'
 class TopStoriesList extends Component{
 
-
+    renderTopStories = () => {
+        return this.props.stories.map((storie) => <TopStorie key={storie["published_date"]} storie={storie}/>) 
+    }
 
     render(){
         return(
-            <div>
-                Hi
+            <div className = 'row'>
+                {this.renderTopStories()}
             </div>
         )
     }
