6989c518d5a5a61180d1394355a8d602cb12f22e
Diego Yanez <diegoyanez@MacBook-Pro-9.fios-router.home>
2019-05-21 21:10:35 -0400

    fix route for top stories

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
-import  { BrowserRouter as Router, Route} from 'react-router-dom'
+import  { BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 
 
-import NavBar from './component/layout/Navbar'
-import Landing from './component/layout/Landing'
+import NavBar from './component/layout/Navbar';
+import Landing from './component/layout/Landing';
+import MostSharedContainer from './containers/MostSharedContainer';
+import TopStoriesContainer from './containers/TopStoriesContainer';
 
 function App() {
   return (
     <div className="App">
     <Router>
       <NavBar/>
-      <Route exact path = "/" component={Landing}/>
+      <Route exact path = "/" component ={ Landing }/>
+      <Route exact path = "/mostshared" component = { MostSharedContainer }/>
+      <Route exact path = "/topstories" component = { TopStoriesContainer }/>
     </Router>
     </div>
   );
